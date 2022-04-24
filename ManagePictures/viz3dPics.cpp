1f3edad7c476357f6d85b1e13810cfa769f17af4
Dietmar Schulz <dietmar@schulzi-family.de>
2022-04-24 18:55:54 +0200

    A picture of a cray geometry excercise

--- ManagePictures/viz3dPics.cpp
+++ ManagePictures/viz3dPics.cpp
@@ -7,17 +7,148 @@ using namespace cv;
 void viz3dPics::showPics(std::string_view imgName)
 {
 	//This will be your reference camera
-	//viz::Viz3d myWindow("Coordinate Frame");
-	//myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
+	viz::Viz3d myWindow("Coordinate Frame");
+	myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
 
 	//Point3d min(0.25, 0.0, 0.25);
 	//Point3d max(0.75, 0.5, 0.75);
-
+	//
+	//// Points bt circles
 	//viz::WCube cube(min, max, false, viz::Color::blue());
-	////cube.setRenderingProperty(viz::LINE_WIDTH, 4.0);
-	//myWindow.showWidget("Cube widget", cube);
+	//viz::WCircle circleZ(0.05);
+	//viz::WCircle circleOne(0.05, Point3d(1.0, 1.0, 1.0), Vec3d(0.0, 0.0, 1.0));
 
-	//myWindow.spin();
+	//viz::WLine line(Point3d(1.0, 1.0, 0.0), Point3d(2.0, 2.0, 2.0), viz::Color::apricot());
+	//viz::WPlane plane(Point3d(1.5, 1.5, 1.5), Vec3d(1.0, 1.0, 1.0), Vec3d(1.0, 0.0, -1.0), Size2d(1.5, 1.5), viz::Color::amethyst());
+	//viz::WGrid planeGrid(Point3d(1.5, 1.5, 1.5), Vec3d(1.0, 1.0, 1.0), Vec3d(1.0, 0.0, -1.0), Vec2i::all(10), Vec2d::all(1.0), viz::Color::amethyst());
+
+	//viz::WSphere sphere(Point3d(1.5, 1.5, 1.5), 1.0, 10, viz::Color::amethyst());
+
+	viz::WGrid grid;
+
+	// Aufgabe
+	Point3d p0(0.0, 0.0, 0.0);
+	Point3d pA(5.0, 5.0, 0.0);
+	viz::WText3D tA("A", pA);
+	Point3d pB(-5.0, 5.0, 0.0);
+	viz::WText3D tB("B", pB);
+	Point3d pC(-5.0, -5.0, 0.0);
+	viz::WText3D tC("C", pC);
+	Point3d pD(5.0, -5.0, 0.0);
+	viz::WText3D tD("D", pD);
+
+	Point3d pE(2.0, 0.0, 4.0);
+	viz::WText3D tE("E", pE);
+	Point3d pF(0.0, 2.0, 4.0);
+	viz::WText3D tF("F", pF);
+	Point3d pG(-2.0, 0.0, 4.0);
+	viz::WText3D tG("G", pG);
+	Point3d pH(0.0, -2.0, 4.0);
+	viz::WText3D tH("H", pH);
+
+	viz::WPolyLine ABCD(std::vector{ pA, pB, pC, pD, pA }, viz::Color::yellow());
+	viz::WPolyLine EFGH(std::vector{ pE, pF, pG, pH, pE }, viz::Color::yellow());
+	EFGH.setRenderingProperty(viz::LINE_WIDTH, 10.0);
+	EFGH.setRenderingProperty(viz::OPACITY, 0.5);
+	EFGH.setRenderingProperty(viz::SHADING, viz::SHADING_GOURAUD);
+	viz::WPolyLine DEAFBGCHD(std::vector{ pD, pE, pA, pF, pB, pG, pC, pH, pD }, viz::Color::yellow());
+
+	viz::WLine E0(pE, p0, viz::Color::maroon());
+	viz::WLine F0(pF, p0, viz::Color::maroon());
+	viz::WLine G0(pG, p0, viz::Color::maroon());
+	viz::WLine H0(pH, p0, viz::Color::maroon());
+	
+	viz::WLine AC(pA, pC, viz::Color::black());
+	viz::WLine BD(pB, pD, viz::Color::black());
+
+	viz::WLine EG(pE, pG, viz::Color::white());
+	viz::WLine FH(pF, pH, viz::Color::white());
+	
+	viz::WLine z(Point3d(0.0, 0.0, -5.0), Point3d(0.0, 0.0, 5.0), viz::Color::red());
+
+	viz::WCircle circle45(8.003, Point3d(0.0, 0.0, -3.75), Vec3d(1.0, 1.0, 0.0));
+	viz::WCircle circle135(8.003, Point3d(0.0, 0.0, -3.75), Vec3d(1.0, -1.0, 0.0));
+	viz::WCircle circle90f(8.003, Point3d(0.0, 0.0, -3.75), Vec3d(1.0, 0.0, 0.0));
+	viz::WCircle circle90(8.003, Point3d(0.0, 0.0, 0.0), Vec3d(1.0, 0.0, 0.0), 0.1, viz::Color::yellow());
+
+	viz::WCircle circle180(8.003, Point3d(0.0, 0.0, -3.75), Vec3d(0.0, 1.0, 0.0));
+	//viz::WSphere sphere(Point3d(0.0, 0.0, -3.75), 8.003, 10, viz::Color::amethyst());
+
+	//sphere.setRenderingProperty(viz::LINE_WIDTH, 4.0);
+	//myWindow.showWidget("Cube widget", cube);
+	//myWindow.showWidget("Circle widget", circleZ);
+	//myWindow.showWidget("CircleOne widget", circleOne);
+	//myWindow.showWidget("Line widget", line);
+	//myWindow.showWidget("Plane widget", plane);
+	myWindow.showWidget("Grid widget", grid);
+	//myWindow.showWidget("PlaneGrid widget", planeGrid);
+	//myWindow.showWidget("Sphere widget", sphere);
+	myWindow.showWidget("ABCD widget", ABCD);
+	myWindow.showWidget("tA widget", tA);
+	myWindow.showWidget("tB widget", tB);
+	myWindow.showWidget("tC widget", tC);
+	myWindow.showWidget("tD widget", tD);
+	myWindow.showWidget("EFGH widget", EFGH);
+	myWindow.showWidget("tE widget", tE);
+	myWindow.showWidget("tF widget", tF);
+	myWindow.showWidget("tG widget", tG);
+	myWindow.showWidget("tH widget", tH);
+	myWindow.showWidget("DEAFBGCHD widget", DEAFBGCHD);
+	myWindow.showWidget("E0 widget", E0);
+	myWindow.showWidget("F0 widget", F0);
+	myWindow.showWidget("G0 widget", G0);
+	myWindow.showWidget("H0 widget", H0);
+	myWindow.showWidget("AC widget", AC);
+	myWindow.showWidget("BD widget", BD);
+	myWindow.showWidget("EG widget", EG);
+	myWindow.showWidget("FH widget", FH);
+	myWindow.showWidget("z widget", z);
+	myWindow.showWidget("circle45 widget", circle45);
+	myWindow.showWidget("circle90f widget", circle90f);
+	myWindow.showWidget("circle135 widget", circle135);
+	myWindow.showWidget("circle180 widget", circle180);
+
+	myWindow.spin();
+
+	std::vector meshPoints{ pA, pB, pC, pD, pE, pF, pG, pH };
+	std::vector faces{ 4, 0, 1, 3, 2, 4, 4, 5, 7, 6, 3, 0, 4, 5, 3, 0, 1, 5, 3, 1, 5, 6, 3, 1, 2, 6, 3, 2, 6, 7, 3, 2, 3, 7, 3, 3, 7, 4, 3, 3, 4, 0 };
+	viz::WMesh mesh(meshPoints, faces);
+	viz::Viz3d  window("Mesh");
+	mesh.setColor(viz::Color::orange_red());
+	mesh.setRenderingProperty(viz::OPACITY, 0.4);
+	mesh.setRenderingProperty(viz::SHADING, viz::SHADING_FLAT);
+	mesh.setRenderingProperty(viz::REPRESENTATION, viz::REPRESENTATION_SURFACE);
+	window.showWidget("mesh", mesh);
+	window.showWidget("circle45 widget", circle45);
+	window.showWidget("circle90 widget", circle90);
+	circle90.setRenderingProperty(viz::LINE_WIDTH, 10);
+	window.showWidget("circle135 widget", circle135);
+	window.showWidget("circle180 widget", circle180);
+	
+	Mat rot_vecZ = Mat::zeros(1, 3, CV_32F);
+	Mat rot_vecY = Mat::zeros(1, 3, CV_32F);
+	
+	while (!window.wasStopped())
+	{
+		/* Rotation using rodrigues */
+		rot_vecZ.at<float>(0, 0) += (float)CV_PI * 0.0f;
+		rot_vecZ.at<float>(0, 1) += (float)CV_PI * 0.0f;
+		rot_vecZ.at<float>(0, 2) += (float)CV_PI * 0.01f;
+
+		rot_vecY.at<float>(0, 0) += (float)CV_PI * 0.0f;
+		rot_vecY.at<float>(0, 1) += (float)CV_PI * 0.01f;
+		rot_vecY.at<float>(0, 2) += (float)CV_PI * 0.0f;
+
+		Mat rot_matZ;
+		Mat rot_matY;
+		Rodrigues(rot_vecZ, rot_matZ);
+		Rodrigues(rot_vecY, rot_matY);
+		Affine3f poseZ(rot_matZ);
+		Affine3f poseY(rot_matY, Vec3d(0.0, 0.0, -3.75));
+		window.setWidgetPose("circle45 widget", poseZ);
+		window.setWidgetPose("circle90 widget", poseY);
+		window.spinOnce(1, true);
+	}
 
 		// Read the camera calibration parameters
 	cv::Mat cameraMatrix;
